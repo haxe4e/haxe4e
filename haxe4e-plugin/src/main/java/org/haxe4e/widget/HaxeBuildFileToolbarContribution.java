3f18456d526d1496fb3e22ba80c8c9cc616f0c16
Ian Harrigan <ianharrigan@hotmail.com>
2021-10-08 14:43:13 +0200

    Initial attempt (#27)
    * Add build file selection combo to toolbar.
    
    * refactor findProjectFromPartRef
    
    * re-add missing image
    
    * detach listener when bundle stops
    
    * dont create prefs each time
    
    Co-authored-by: sebthom <sebthom@users.noreply.github.com>

---
+++ haxe4e-plugin/src/main/java/org/haxe4e/widget/HaxeBuildFileToolbarContribution.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2021 by the Haxe4E authors.
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.haxe4e.widget;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CCombo;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Cursor;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.menus.WorkbenchWindowControlContribution;
+import org.haxe4e.project.HaxeProject;
+import org.haxe4e.util.LOG;
+import org.haxe4e.util.ui.UI;
+
+/**
+ * @author Ian Harrigan
+ */
+public class HaxeBuildFileToolbarContribution extends WorkbenchWindowControlContribution implements SelectionListener {
+   public static HaxeBuildFileToolbarContribution instance;
+
+   public static IProject currentProject;
+   private CCombo hxmlList;
+
+   public HaxeBuildFileToolbarContribution() {
+      instance = this;
+   }
+
+   @Override
+   public boolean isDynamic() {
+      return true;
+   }
+
+   @Override
+   protected Control createControl(final Composite parent) {
+      final var container = new Composite(parent, SWT.NONE);
+      final var gridLayout = new GridLayout(1, false);
+      gridLayout.marginLeft = 7;
+      gridLayout.marginTop = 0;
+      gridLayout.marginHeight = 0;
+      gridLayout.marginWidth = 0;
+      container.setLayout(gridLayout);
+      final var gridData = new GridData(SWT.FILL, SWT.FILL, false, false, 1, 1);
+      gridData.widthHint = 120;
+      hxmlList = new CCombo(container, SWT.BORDER | SWT.DROP_DOWN | SWT.READ_ONLY | SWT.NO_FOCUS | SWT.SINGLE);
+      hxmlList.setLayoutData(gridData);
+      hxmlList.addSelectionListener(this);
+
+      // even read-only dropdown shows I-beam cursor - lets fix that
+      final var shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+      final var cursor = new Cursor(shell.getDisplay(), SWT.CURSOR_ARROW);
+      hxmlList.setCursor(cursor);
+
+      return container;
+   }
+
+   public void refresh(final IProject project) {
+      if (project == null)
+         return;
+
+      if (project == currentProject)
+         return;
+
+      if (hxmlList.getListVisible())
+         return;
+
+      try {
+         currentProject = project;
+
+         final var haxeProject = new HaxeProject(project);
+         var currentBuildFile = haxeProject.getPrefs().getHaxeBuildFile();
+         final var buildFiles = haxeProject.getBuildFiles();
+         if ((currentBuildFile == null || currentBuildFile.trim().length() == 0) && buildFiles.size() > 0) {
+            currentBuildFile = buildFiles.get(0);
+            haxeProject.setBuildFile(currentBuildFile);
+         }
+         hxmlList.removeAll();
+         hxmlList.setItems(buildFiles.toArray(new String[0]));
+         hxmlList.setText(currentBuildFile);
+      } catch (final Exception ex) {
+         LOG.error(ex);
+      }
+   }
+
+   @Override
+   public void widgetSelected(final SelectionEvent event) {
+      handleSelectionChanged();
+   }
+
+   @Override
+   public void widgetDefaultSelected(final SelectionEvent e) {
+      handleSelectionChanged();
+   }
+
+   private void handleSelectionChanged() {
+      if (currentProject != null) {
+         final var newHxmlFile = hxmlList.getItem(hxmlList.getSelectionIndex());
+         final var haxeProject = new HaxeProject(currentProject);
+
+         haxeProject.setBuildFile(newHxmlFile);
+
+         // dropdown steals focus, lets set it back to any active editor
+         final var editor = UI.getActiveEditor();
+         if (editor != null) {
+            editor.setFocus();
+         }
+      }
+   }
+}
