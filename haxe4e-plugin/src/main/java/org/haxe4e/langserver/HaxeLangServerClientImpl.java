f5c952f942a2bd16b0242e106de9dc065196f1ef
sebthom <sebthom@users.noreply.github.com>
2022-10-17 21:14:32 +0200

    Fix progress manager shows never ending Language Server jobs

--- haxe4e-plugin/src/main/java/org/haxe4e/langserver/HaxeLangServerClientImpl.java
+++ haxe4e-plugin/src/main/java/org/haxe4e/langserver/HaxeLangServerClientImpl.java
@@ -4,31 +4,14 @@
  */
 package org.haxe4e.langserver;
 
-import static net.sf.jstuff.core.validation.NullAnalysisHelper.*;
-
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CountDownLatch;
 
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.lsp4e.LanguageClientImpl;
 import org.eclipse.lsp4j.DidChangeConfigurationParams;
-import org.eclipse.lsp4j.ProgressParams;
 import org.eclipse.lsp4j.RegistrationParams;
-import org.eclipse.lsp4j.WorkDoneProgressBegin;
-import org.eclipse.lsp4j.WorkDoneProgressCreateParams;
-import org.eclipse.lsp4j.WorkDoneProgressEnd;
-import org.eclipse.lsp4j.jsonrpc.messages.Either;
 import org.haxe4e.Haxe4EPlugin;
 import org.haxe4e.util.TreeBuilder;
-
-import net.sf.jstuff.core.concurrent.Threads;
 
 /**
  * @author Sebastian Thomschke
@@ -37,59 +20,6 @@ import net.sf.jstuff.core.concurrent.Threads;
 public final class HaxeLangServerClientImpl extends LanguageClientImpl implements HaxeLangServerClient {
 
    private boolean isInitTriggered = false;
-
-   private final Map<@NonNull Either<?, ?>, CountDownLatch> progressMonitors = new HashMap<>();
-
-   @Override
-   public @NonNullByDefault({}) CompletableFuture<Void> createProgress(final WorkDoneProgressCreateParams params) {
-      Haxe4EPlugin.log().debug("createProgress: {0}", params);
-      return CompletableFuture.completedFuture(null);
-   }
-
-   @Override
-   public void notifyProgress(final @NonNullByDefault({}) ProgressParams params) {
-      synchronized (progressMonitors) {
-         if (params.getValue().isLeft()) {
-            final var notification = params.getValue().getLeft();
-            if (notification instanceof final WorkDoneProgressBegin progressBegin) {
-               final var signal = new CountDownLatch(1);
-               final var job = new Job(progressBegin.getTitle()) {
-                  @Override
-                  protected void canceling() {
-                     getThread().interrupt();
-                  }
-
-                  @Override
-                  protected IStatus run(final IProgressMonitor monitor) {
-                     monitor.beginTask(progressBegin.getTitle(), IProgressMonitor.UNKNOWN);
-                     try {
-                        signal.await();
-                     } catch (final InterruptedException ex) {
-                        Threads.handleInterruptedException(ex);
-                        monitor.setCanceled(true);
-                     } finally {
-                        monitor.done();
-                     }
-                     synchronized (progressMonitors) {
-                        progressMonitors.remove(params.getToken());
-                     }
-                     return Status.OK_STATUS;
-                  }
-               };
-               job.setPriority(Job.INTERACTIVE);
-               job.schedule();
-               progressMonitors.put(asNonNull(params.getToken()), signal);
-            } else if (notification instanceof WorkDoneProgressEnd) {
-               final var signal = progressMonitors.get(params.getToken());
-               if (signal != null) {
-                  signal.countDown();
-               }
-            }
-         } else {
-            Haxe4EPlugin.log().debug("notifyProgress: {0}", params);
-         }
-      }
-   }
 
    @Override
    public void onHaxeCacheBuildFailed(final Object value) {
